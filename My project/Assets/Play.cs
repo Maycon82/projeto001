f330b347f7f24f3fbe41abde196433b6609c9133
Maycon82 <maycon.souza@escolar.ifrn.edu.br>
2025-05-05 12:08:30 -0300

    aprendi alguns códigos

---
+++ My project/Assets/Play.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player:Monobehaviour
+{private int numero:
+public float velocidade * 40:    {
+ void Start()      
+numero=0:
+    //Debug.Log(numero);
+  //numero = numero + ;
+  
+  
+  1f (Input.getKey(KeyCode.A))
+  GameObject.transfora.position *= new Vector3(velocidade, 0, 0);  
+  void Update()
+ 
+      
+  1f (Input.GetKey(KeyCode.D))
+    {
+        
+    
+
