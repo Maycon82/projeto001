f696e87952d17888a8ab9516240eadd4618f9794
Maycon82 <maycon.souza@escolar.ifrn.edu.br>
2025-05-12 12:01:39 -0300

    aprendi como se faz para obter o resultado da soma

---
+++ My project/Assets/testesoma.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class testesoma : MonoBehaviour
+{
+    private int A;
+    private int B;
+    private int C;
+    void Start()
+    {
+
+
+        A = 45;
+        B = 32;
+        C = A + B;
+
+        Debug.Log(C);
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
