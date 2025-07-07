6de70c9fdd03f298fdac1dcf112e418c7505f989
Maycon82 <maycon.souza@escolar.ifrn.edu.br>
2025-07-07 12:04:05 -0300

    zzz
    pulo básica!!!

--- My project/Assets/Play.cs
+++ My project/Assets/Play.cs
@@ -1,41 +1,69 @@
+using System;
 using UnityEngine;
 
-public class Play : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    
     public float velocidade = 40;
     public float forcaDoPulo = 4;
-
+    
+    private bool noChao = false;
+    private bool andando = false;
+    
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
-
-
+    private Animator animator;
+    
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
     }
 
-
-
+       
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
-        }
-
+        andando = false;
+        
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
-            sprite.flipX = false;
-
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddForce(new Vector2(0, forcaDoPulo), ForceMode2D.Impulse);
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
+            sprite.flipX = true;
+            andando = true;
         }
         
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+            sprite.flipX = false;
+            andando = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
+        {
+            rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
+        }
+
+        animator.SetBool("Andando",andando);
+        animator.SetBool("Pulo",!noChao);
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D colisao)
+    {
+        //if (colisao.gameObject.tag == "Chao")
+        if(colisao.gameObject.CompareTag("Chao"))
+        {
+            noChao = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D colisao)
+    {
+        if(colisao.gameObject.CompareTag("Chao"))
+        {
+            noChao = false;
+        }
     }
 }
